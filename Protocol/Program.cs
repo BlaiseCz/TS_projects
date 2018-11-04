45aa2b4822b2e10275d802a0e709b8973c9b434e
Krzysztof <czarneckikrzysztof666@gmail.com>
2018-11-04 19:44:54 +0100

    Początkowe zaewidencjonowanie modułu Protocol

---
+++ Protocol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Protocol
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
