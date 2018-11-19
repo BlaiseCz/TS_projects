945c42634181459fc4747d22e990dd1f04d2526e
Krzysztof Czarnecki <33467432+Krzysiulek@users.noreply.github.com>
2018-11-19 18:56:07 +0100

    Client v0.2

--- ClientUDP/Client_Asyn_Main.cs
+++ ClientUDP/Client_Asyn_Main.cs
@@ -9,18 +9,35 @@ namespace Client_Asyn
 {
     class Client
     {
+        //kody pol operacji
+        const uint ID_REQUST = 1; //wysyla jesli klient chce nadania ID
+        const uint ID_SENT = 2; //odbiera takie polecenie, jezeli nadano ID
+        const uint NUMB_SEND = 3; //wysyla jesli klient wyslal liczbe
+        const uint ANSWER_CONFIRM = 4; //odbiera i odpowiada na pytanie czy zgadnieto
+        const uint END_CONNCECTION = 5; //jesli klient opuszcza polaczenie wysylana jest taka informacja
+        const uint TEN_SEC_REMIND = 6; //wywołanie przypomnienia
+        const uint TIME_END = 7; //odbiera jesli w zadanym czasie nie zgadnieto
+        const uint NUMB_REQUEST = 8; //wysylane jesli chce, aby klient wyslal liczbe
+        const uint SECND_CLIENT_AWAIT = 9; //wysylane jezeli nie jest polaczony drugi klient
+
         IntOperations operations = new IntOperations();
-        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0); //endpoint
         readonly string IP_ADDR_SERVER = "127.0.0.1";
         readonly int PORT = 8080;
+        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0); //endpoint
         private UdpClient client; //deklaracja clienta UDP
-        //private readonly int BYTES_TO_SEND = 4; //jaka dlugosc pola danych w bajtach chcemy wyslac
+        private readonly int BYTES_TO_SEND = 3; //jaka dlugosc pola danych w bajtach chcemy wyslac
         uint ID; //ID klienta
-        //public List<ReceivedData> receiveds = new List<ReceivedData>();
+        public List<ReceivedData> receiveds = new List<ReceivedData>();
 
 
         Client(){
-            client = new UdpClient(IP_ADDR_SERVER, PORT);
+            Console.WriteLine("Podaj Adres IP serwera ");
+            IP_ADDR_SERVER = Console.ReadLine();
+
+            Console.WriteLine("Podaj Port ");
+            PORT = Convert.ToInt32(Console.ReadLine());
+
+            client = new UdpClient(IP_ADDR_SERVER, PORT); //tworzy klienta
             ID = 0; //narazie na sztywno
             Console.WriteLine(RemoteIpEndPoint.AddressFamily);
         }
@@ -37,20 +54,60 @@ namespace Client_Asyn
             {
                 IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0); //endpoint
                 byte[] receivedBytes = client.Receive(ref RemoteIpEndPoint); //bajty, do których zapisujemy to co otrzymalismy
-                operations.printAllFields(ref receivedBytes); //drukuje to co otrzymal
+                //operations.printAllFields(ref receivedBytes); //drukuje to co otrzymal
 
+                receiveds.Add(new ReceivedData(receivedBytes, RemoteIpEndPoint)); //wrzuca otrzymany wynik razem z EndPointerm do Tablicy Oderbanych danych
+            }
+        }
 
-                //receiveds.Add(new ReceivedData(receivedBytes, RemoteIpEndPoint)); //wrzuca otrzymany wynik razem z EndPointerm do Tablicy Oderbanych danych
+        //do faktycznej obslugi
+        private void OperateRequest(ReceivedData data)
+        {
+            byte[] recvd_data = data.getData();
+            IPEndPoint endPoint = data.GetEndPoint();
+
+            uint operation = operations.GetOperation(ref recvd_data);
+            switch (operation)
+            {
+                case ID_SENT:
+                    Console.WriteLine("Wyslano ID");
+                    ID = operations.GetID(ref recvd_data);
+                    break;
+
+                case NUMB_REQUEST:
+                    Console.WriteLine("Podaj liczbe: ");
+                    uint Answer = Convert.ToUInt32(Console.ReadLine());
+                    Send(Answer, NUMB_SEND);
+                    break;
             }
         }
 
         private void ManageRequests()
         {
-            Console.WriteLine("Request Manager Started");
+            //Console.WriteLine("Request Manager Started");
             IntOperations operations = new IntOperations();
             byte[] recvd_data = new byte[3];
             //jezeli dane maja ACK = 1 nic z nimi nie rob
             //jezeli inaczej - rob to co masz zrobic i odeslij to samo z ACK
+
+            if (receiveds.Count > 0) //jezeli sa dane w kontenerze zapisanych odebranych danych
+            {
+                recvd_data = receiveds[0].getData(); //wyłuskanie otrzymanych danych
+
+                if (operations.GetACK(ref recvd_data) == 0) //jezeli ACK = 0 i ID 
+                {
+                    operations.SetACK(ref recvd_data, 1); //ustawia flage ACK na 1 i odsyla do serwera
+                    client.Send(recvd_data, BYTES_TO_SEND); // -//-
+                    OperateRequest(receiveds[0]); //przechodze do wlasciwej funkcji obslugi requestow - sciagam ostatni request
+                }
+
+                else //jezeli ACK = 1, no to nie robi nic
+                {
+                    Console.WriteLine("Serwer cos potwierdzil");
+                }
+
+                receiveds.RemoveAt(0); // usuwa, bo zostalo obsluzone
+            }
         }
 
         public static void Main(string[] args)
@@ -64,12 +121,9 @@ namespace Client_Asyn
 
             client.Send(0, 1); //prosba o uzyskanie ID sesji
 
-
-            while (true)
-            {
-
-                byte[] tmp = new byte[3];
-
+            while(true){
+                Thread.Sleep(1000);
+                client.ManageRequests();
             }
         }
     }
---
+++ ClientUDP/ReceivedData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace Client_Asyn
+{
+    public class ReceivedData
+    {
+        byte[] receivedData;
+        IPEndPoint endPoint;
+
+        public ReceivedData(byte[] data, IPEndPoint endPoint)
+        {
+            this.receivedData = data;
+            this.endPoint = endPoint;
+        }
+
+        public byte[] getData(){
+            return this.receivedData;
+        }
+
+        public IPEndPoint GetEndPoint(){
+            return this.endPoint;
+        }
+    }
+}
