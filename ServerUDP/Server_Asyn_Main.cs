b14ed41bef402017de0976232a4940998745de4c
Krzysztof Czarnecki <33467432+Krzysiulek@users.noreply.github.com>
2018-11-20 01:09:38 +0100

    POPRAWA STARTU KLIENTOW
    Poprawilem start klientow - sprawdzenie tego odbywa sie w dalszej czesci ManageRequest
    
    Poprawilem: niepotrzebne żadanie liczby przy nadaniu ID, brak żądania nowej liczby przy nieoprawnej licznie i niepotrzebne żądanie liczby przy poprawnej liczbie

--- ServerUDP/Server_Asyn_Main.cs
+++ ServerUDP/Server_Asyn_Main.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
@@ -28,10 +28,11 @@ namespace Server_Asyn
         private UdpClient client; //deklaracja clienta UDP
         private readonly int BYTES_TO_SEND = 3; //jaka dlugosc pola danych w bajtach chcemy wyslac
 
-
+        const uint HOW_MANY_CLIENTS_TO_START_THIS_GAME = 1; // :)
         List<ConnectedClient> clients = new List<ConnectedClient>(); //lista przechowujaca polaczonych klientow - ID i endpoint
         private List<ReceivedData> receiveds = new List<ReceivedData>(); //list przechowujaca dane odebrane od klientow
         private uint secretNumber = 11; //liczba tajna - dorobic metode generujaca ja z przedzialu 0 - 15
+        bool IsGameStarted = false;
 
         //konstuktor
         Server()
@@ -58,8 +59,9 @@ namespace Server_Asyn
         {
             byte[] recvd_data = data.getData(); //wyluskuje odebrane bajty 
             IPEndPoint endPoint = data.GetEndPoint(); //wyluskuje endpoint z ktorego zostala informacja odebrana
-            uint ID;
+            uint ID = operations.GetID(ref recvd_data); //WYSTARCZY RAZ JAK UZYSKASZ ID, NIE MUSISZ W KAZDEJ OPERACJI, TA METODA SIE WYWOLUJE ZA KAZDYM REQUESTEM OD NOWA
             uint operation = operations.GetOperation(ref recvd_data);
+
             switch (operation)
             {
                 //Obsluga requestu dot. nadania ID
@@ -67,7 +69,7 @@ namespace Server_Asyn
                     Console.WriteLine("Request for ID..");
                     uint generatedID = GenerateID(); //generuje ID
                     Send(0, ID_SENT, generatedID, endPoint); //wysyla z poleceniem ID_SENT
-                    Send(0, NUMB_REQUEST, generatedID, endPoint);
+                    //Send(0, NUMB_REQUEST, generatedID, endPoint); - ZĄDANIE O LICZBĘ NIE TUTAJ, BO MOŻE BYĆ <2 KLIENTOW
                     clients.Add(new ConnectedClient(generatedID, endPoint)); //tutaj dodaje do mapy endpoint i ID sesji
                     Console.WriteLine("Done..");
                     //drukuje dodanych do struktury klientow:
@@ -81,43 +83,39 @@ namespace Server_Asyn
                 case NUMB_RCV:
                     //odsyla info z operacja ANSWER_CONFIRM i uzupelnionym polem operacji odpowiednio
                     uint received_num = operations.GetAnswer(ref recvd_data);
-                    ID = operations.GetID(ref recvd_data);
                     if (received_num == secretNumber)
                     {
                         Console.WriteLine("Correct number! :)");
                         Send(1, ANSWER_CONFIRM, ID, endPoint);
-                        Send(0, NUMB_REQUEST, ID, endPoint);
+                        //Send(0, NUMB_REQUEST, ID, endPoint); //PO PORAWNIE PODANEJ LICZBIE NIE CHCE ODEBRAC INFORMACJI O TYM, ŻE MAM KOLEJNA LICZBE WYSLAC
                     }
                     else
                     {
                         Console.WriteLine("Wrong number :( ");
                         Send(0, ANSWER_CONFIRM, ID, endPoint);
+                        Send(0, NUMB_REQUEST, ID, endPoint);
                     }
                     break;
 
                 case END_CONNCECTION: //jesli klient opuszcza polaczenie wysylana jest taka informacja
-                        Console.WriteLine("Client leaves..");
-                        ID = operations.GetID(ref recvd_data);
-                        Send(1, END_CONNCECTION, ID, endPoint);
-                        Console.WriteLine("Client left..");
+                    Console.WriteLine("Client leaves..");
+                    Send(1, END_CONNCECTION, ID, endPoint);
+                    Console.WriteLine("Client left..");
                     break;
 
                 case TEN_SEC_REMIND:
-                        Console.WriteLine("10s reminder..");
-                        ID = operations.GetID(ref recvd_data);
-                        Send(0, TEN_SEC_REMIND, ID, endPoint);
+                    Console.WriteLine("10s reminder..");
+                    Send(0, TEN_SEC_REMIND, ID, endPoint);
                     break;
 
                 case TIME_END:
-                        Console.WriteLine("Time's up!");
-                        ID = operations.GetID(ref recvd_data);
-                        Send(0, TIME_END, ID, endPoint);
+                    Console.WriteLine("Time's up!");
+                    Send(0, TIME_END, ID, endPoint);
                     break;
-         
+
                 case SECND_CLIENT_AWAIT:
-                        Console.WriteLine("Waiting for second client...");
-                        ID = operations.GetID(ref recvd_data);
-                        Send(0, SECND_CLIENT_AWAIT, ID, endPoint);
+                    Console.WriteLine("Waiting for second client...");
+                    Send(0, SECND_CLIENT_AWAIT, ID, endPoint);
                     break;
             }
         }
@@ -146,6 +144,20 @@ namespace Server_Asyn
 
                 receiveds.RemoveAt(0); // usuwa ostatni request, bo zostalo obsluzone
             }
+
+            //SPRAWDZA CZY MOZNA WYSTARTOWAC GRE
+            if (IsGameStarted == false)
+            {
+                if (clients.Count >= HOW_MANY_CLIENTS_TO_START_THIS_GAME)
+                {
+                    IsGameStarted = true;
+                    foreach (ConnectedClient cl in clients)
+                        Send(0, NUMB_REQUEST, cl.getID(), cl.getEndpoint()); //w tym miejscu wysyla request, zeby klienci wyslali liczbe
+                    IsGameStarted = true;
+                }
+                else
+                    return;
+            }
         }
 
         public void Receive()
@@ -156,19 +168,19 @@ namespace Server_Asyn
                 byte[] receivedBytes = client.Receive(ref RemoteIpEndPoint); //bajty, do których zapisujemy to co otrzymalismy
                 operations.printImpFields(ref receivedBytes); //drukuje to co otrzymal
                 receiveds.Add(new ReceivedData(receivedBytes, RemoteIpEndPoint)); //wrzuca otrzymany wynik razem z EndPointerm do Tablicy Oderbanych danych
-                Console.WriteLine("Odebrano");
+                Console.WriteLine("Received...");
             }
         }
 
         //jezeli dwoch klientow bedzie polaczonych, to trzeba wystartować watek z ta metoda
         public void Inform()
         {
-            uint TimeLeft = 0; //tutaj trzeba zachować czas do końca sesji
-            Thread.Sleep(10000); //usypia watek na 10sek.
-            foreach (ConnectedClient cl in clients) //do kazdego połączonego klienta wysyla info, że zostalo TimeLeft czasu do konca rozgrywki
-            {
-                Send(TimeLeft, TEN_SEC_REMIND, cl.getID(), cl.getEndpoint());
-            }
+                Thread.Sleep(10000); //usypia watek na 10sek
+                uint TimeLeft = 0; //tutaj trzeba zachować czas do końca sesji
+                foreach (ConnectedClient cl in clients) //do kazdego połączonego klienta wysyla info, że zostalo TimeLeft czasu do konca rozgrywki
+                {
+                    Send(TimeLeft, TEN_SEC_REMIND, cl.getID(), cl.getEndpoint());
+                }
         }
 
 
@@ -181,20 +193,15 @@ namespace Server_Asyn
             Thread recv_thr = new Thread(server.Receive); //tworzy watek nasłuchujacy
             recv_thr.Start(); //startuje nasluchujacy watek
 
+            Thread TenSecRemind = new Thread(server.Inform); //ten watek do dopracowania
+            //TenSecRemind.Start();
 
             while (true)
             {
                 //Console.Write(".");
                 Thread.Sleep(1000); //a to po to, żeby się to wszystko tak szybko w konsoli nie działo - można zmniejszyc docelowo
 
-                if (server.clients.Count > 1) //jezeli podlaczonych jest wiecej niz jeden klient mozna startowac z obsluga
-                    server.ManageRequests();
-
-                else //jezeli nie podlaczonych jest dwoch klientow, trzeba przeslac do ewentualnych klientow, że czekamy
-                {
-                    Thread.Sleep(1000); //zeby nie zasypywac klienta spamem o tym, że czekamy na drugiego klienta
-                    //wyslij informacje o tym, ze czekamy na drugiego klienta - kod operacji SECND_CLIENT_AWAIT
-                }
+                server.ManageRequests(); //jezeli podlaczonych jest wiecej niz jeden klient mozna startowac z obsluga
 
             }
         }
