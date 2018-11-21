82f53dd1fb136f4de99691a7f5955475c7537092
Błażej <44238271+MamoToJaa@users.noreply.github.com>
2018-11-21 19:47:09 +0100

    Add files via upload

---
+++ server.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server_tcp
+{
+    public class server
+    {
+        public server()
+        {
+            TcpClient server = new TcpClient();
+        }
+    }
+}
---
+++ server_main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server_tcp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            server Server = new server();
+
+            while(true)
+            {
+
+            }
+        }
+    }
+}
