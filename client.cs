3d6d714ab34f2db450dd99bb07906c5e88ed10ef
Błażej <44238271+MamoToJaa@users.noreply.github.com>
2018-11-21 19:46:25 +0100

    Add files via upload

---
+++ client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client_tcp
+{
+    public class client
+    {
+        public client()
+        {
+        }
+    }
+}
---
+++ client_main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client_tcp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            client Client = new client();
+
+            while(true)
+            {
+
+            }
+        }
+    }
+}
